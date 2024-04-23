cea9a8d498e947229688ff8441e9172087dacdf2
lulokent <cwengalulonkem@gmail.com>
2024-04-23 01:23:37 -0700

    files updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,18 +2,13 @@
 # using Puppet to make changes to configuration file.
 # accessing ssh configuarion file without using a password.
 
-file { '/etc/ssh/ssh_config':
-	ensure => present,
-}
 
 file_line { 'Turn off passwd auth':
 	path   => '/etc/ssh/ssh_config',
-	line   => 'IdentityFile ~/.ssh/school',
-	match  => '^#IdentityFile'.
+	line   => 'PasswordAuthentication no'
 }
 
 file_line { 'Declare identity file':
 	path   => '/etc/ssh/ssh_config',
-	line   => 'IdentityFile ~/.ssh/school',
-	match  => '^#IdentityFile',
+	line   => 'IdentityFile ~/.ssh/school'
 }
