fd9317c5d53fd1b41a309930521e0c9cc7dfab1d
Cwenga  Mkentane <144338571+lulokent@users.noreply.github.com>
2024-11-04 10:46:33 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,7 +0,0 @@
-# Installs flask version 2.1.0 using pip3
-
-package { 'flask':
-	ensure   => '2.1.0',
-	name     => 'flask',
-	provider => 'pip3'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# Kills a process named killmenow using Puppet
-
-exec { 'pkill':
-	command => 'pkill killmenow',
-	returns => [0,1]
-}
-
